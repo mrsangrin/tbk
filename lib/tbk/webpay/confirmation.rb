3c9c50b504673fa3e852c42fb0f837dce9bd733b
Sandro Ariel Gómez Araya <sandro.gomez@gmail.com>
2014-08-18 17:15:02 -0400

    add payment types

--- lib/tbk/webpay/confirmation.rb
+++ lib/tbk/webpay/confirmation.rb
@@ -1,122 +1,134 @@
 # encoding: UTF-8
 module TBK
-  module Webpay
-    class Confirmation
-      RESPONSE_CODES = {
-        '0' => 'Transacción aprobada.',
-        '-1' => 'Rechazo de tx. en B24, No autorizada',
-        '-2' => 'Transacción debe reintentarse.',
-        '-3' => 'Error en tx.',
-        '-4' => 'Rechazo de tx. En B24, No autorizada',
-        '-5' => 'Rechazo por error de tasa.',
-        '-6' => 'Excede cupo máximo mensual.',
-        '-7' => 'Excede límite diario por transacción.',
-        '-8' => 'Rubro no autorizado.'
-      }
+	module Webpay
+		class Confirmation
+			RESPONSE_CODES = {
+				'0' => 'Transacción aprobada.',
+				'-1' => 'Rechazo de tx. en B24, No autorizada',
+				'-2' => 'Transacción debe reintentarse.',
+				'-3' => 'Error en tx.',
+				'-4' => 'Rechazo de tx. En B24, No autorizada',
+				'-5' => 'Rechazo por error de tasa.',
+				'-6' => 'Excede cupo máximo mensual.',
+				'-7' => 'Excede límite diario por transacción.',
+				'-8' => 'Rubro no autorizado.'
+			}
+			PAYMENT_TYPE_CODES = {
+				'VN' => 'Venta Normal',
+				'VC' => 'Venta Cuotas',
+				'SI' => 'Tres Cuotas Sin Interes',
+				'S2' => 'Dos Cuotas Sin Interes',
+				'CI' => 'Cuotas Comercio'
 
-      attr_accessor :commerce
-      attr_accessor :request_ip
-      attr_reader :raw
-      attr_reader :params
+			}
 
-      def initialize(options)
-        options = { :body => options } if options.is_a?(String)
+			attr_accessor :commerce
+			attr_accessor :request_ip
+			attr_reader :raw
+			attr_reader :params
 
-        self.commerce = options[:commerce] || TBK::Commerce.default_commerce
-        self.request_ip = options[:request_ip]
-        self.parse(options[:body])
-      end
+			def initialize(options)
+				options = { :body => options } if options.is_a?(String)
 
-      def acknowledge
-        self.commerce.webpay_encrypt('ACK')
-      end
+				self.commerce = options[:commerce] || TBK::Commerce.default_commerce
+				self.request_ip = options[:request_ip]
+				self.parse(options[:body])
+			end
 
-      def reject
-        self.commerce.webpay_encrypt('ERR')
-      end
+			def acknowledge
+				self.commerce.webpay_encrypt('ACK')
+			end
 
-      def success?
-        self.params[:TBK_RESPUESTA] == "0"
-      end
+			def reject
+				self.commerce.webpay_encrypt('ERR')
+			end
 
-      def order_id
-        self.params[:TBK_ORDEN_COMPRA]
-      end
+			def success?
+				self.params[:TBK_RESPUESTA] == "0"
+			end
 
-      def session_id
-        self.params[:TBK_ID_SESION]
-      end
+			def order_id
+				self.params[:TBK_ORDEN_COMPRA]
+			end
 
-      def transaction_id
-        self.params[:TBK_ID_TRANSACCION]
-      end
+			def session_id
+				self.params[:TBK_ID_SESION]
+			end
 
-      def amount
-        self.params[:TBK_MONTO].to_f/100
-      end
+			def transaction_id
+				self.params[:TBK_ID_TRANSACCION]
+			end
 
-      def authorization
-        self.params[:TBK_CODIGO_AUTORIZACION]
-      end
+			def amount
+				self.params[:TBK_MONTO].to_f/100
+			end
 
-      def signature
-        self.params[:TBK_MAC]
-      end
+			def authorization
+				self.params[:TBK_CODIGO_AUTORIZACION]
+			end
 
-      def card_display_number
-        "XXXX-XXXX-XXXX-#{ card_last_numbers }"
-      end
+			def signature
+				self.params[:TBK_MAC]
+			end
 
-      def card_last_numbers
-        self.params[:TBK_FINAL_NUMERO_TARJETA]
-      end
+			def card_display_number
+				"XXXX-XXXX-XXXX-#{ card_last_numbers }"
+			end
 
-      def message
-        RESPONSE_CODES[self.params[:TBK_RESPUESTA]]
-      end
+			def card_last_numbers
+				self.params[:TBK_FINAL_NUMERO_TARJETA]
+			end
 
-      def paid_at
-        @paid_at ||= begin
-          year = Time.now.year
-          month = self.params[:TBK_FECHA_TRANSACCION][0...2].to_i
-          day = self.params[:TBK_FECHA_TRANSACCION][2...4].to_i
-          hour = self.params[:TBK_HORA_TRANSACCION][0...2].to_i
-          minutes = self.params[:TBK_HORA_TRANSACCION][2...4].to_i
-          seconds = self.params[:TBK_HORA_TRANSACCION][4...6].to_i
+			def message
+				RESPONSE_CODES[self.params[:TBK_RESPUESTA]]
+			end
 
-          offset = if defined? TZInfo::Timezone
-            # Use tzinfo gem if available
-            TZInfo::Timezone.get('America/Santiago').period_for_utc(DateTime.new(year,month,day,hour,minutes,0)).utc_offset
-          else
-            -14400
-          end
+			def payment_type
+				PAYMENT_TYPE_CODES[self.params[:TBK_TIPO_PAGO]]
+			end
 
-          Time.new(year, month, day, hour, minutes, seconds, offset)
-        end
-      end
+			def paid_at
+				@paid_at ||= begin
+					year = Time.now.year
+					month = self.params[:TBK_FECHA_TRANSACCION][0...2].to_i
+					day = self.params[:TBK_FECHA_TRANSACCION][2...4].to_i
+					hour = self.params[:TBK_HORA_TRANSACCION][0...2].to_i
+					minutes = self.params[:TBK_HORA_TRANSACCION][2...4].to_i
+					seconds = self.params[:TBK_HORA_TRANSACCION][4...6].to_i
+
+					offset = if defined? TZInfo::Timezone
+						# Use tzinfo gem if available
+						TZInfo::Timezone.get('America/Santiago').period_for_utc(DateTime.new(year,month,day,hour,minutes,0)).utc_offset
+					else
+						-14400
+					end
+
+					Time.new(year, month, day, hour, minutes, seconds, offset)
+				end
+			end
 
 
-      protected
-        def parse(post)
-          @raw = post.to_s
-          @raw_params = {}
-          for line in @raw.split('&')
-            key, value = *line.scan( %r{^([A-Za-z0-9_.]+)\=(.*)$} ).flatten
-            @raw_params[key] = CGI.unescape(value)
-          end
+			protected
+				def parse(post)
+					@raw = post.to_s
+					@raw_params = {}
+					for line in @raw.split('&')
+						key, value = *line.scan( %r{^([A-Za-z0-9_.]+)\=(.*)$} ).flatten
+						@raw_params[key] = CGI.unescape(value)
+					end
 
-          @params = {}
-          decrypted_params = self.commerce.webpay_decrypt(@raw_params['TBK_PARAM'])
-          for line in decrypted_params[:body].split('#')
-            key, value = *line.scan( %r{^([A-Za-z0-9_.]+)\=(.*)$} ).flatten
-            @params[key.to_sym] = CGI.unescape(value)
-          end
-          @params[:TBK_MAC] = decrypted_params[:signature]
+					@params = {}
+					decrypted_params = self.commerce.webpay_decrypt(@raw_params['TBK_PARAM'])
+					for line in decrypted_params[:body].split('#')
+						key, value = *line.scan( %r{^([A-Za-z0-9_.]+)\=(.*)$} ).flatten
+						@params[key.to_sym] = CGI.unescape(value)
+					end
+					@params[:TBK_MAC] = decrypted_params[:signature]
 
-          TBK::Webpay.logger.confirmation(self)
+					TBK::Webpay.logger.confirmation(self)
 
-          true
-        end
-    end
-  end
+					true
+				end
+		end
+	end
 end
